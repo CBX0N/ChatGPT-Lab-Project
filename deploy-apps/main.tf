f1a22a684116d3b70db0152ddb27508589d8fb19
charlie boon <charlie.boon@sky.com>
2024-10-23 12:08:08 +0100

    adding helm tf deploy

--- deploy-apps/main.tf
+++ deploy-apps/main.tf
@@ -8,7 +8,6 @@ resource "helm_release" "argocd" {
 
   set {
     name  = "configs.secret.argocdServerAdminPassword"
-    value = "$2a$10$dQlb1epH4xIDue7b08Qqnuyy5dLoSKOwwVRxpS0xAtpifbNGIOgSW"
+    value = var.argocd_server_admin_password
   }
 }
-
---
+++ deploy-apps/variables.tf
@@ -0,0 +1,4 @@
+variable "argocd_server_admin_password" {
+  type = string
+  sensitive = true
+}
